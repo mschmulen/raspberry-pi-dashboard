90906fd1e740cbcb8a4286eae0e5dc1169e04a4b
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-08 11:10:41 -0800

    pathing

--- refresh.sh
+++ refresh.sh
@@ -9,15 +9,22 @@ sudo chmod 777 start
 sudo wget -N http://raw.github.com/mschmulen/raspberry-pi-dashboard/master/stop
 sudo chmod 777 stop
 
+#finally update the refresh script itself
+#sudo wget -N http://raw.github.com/mschmulen/raspberry-pi-dashboard/master/refresh.sh
+#sudo chmod +x refresh.sh
+
+cd /etc/init.d/
+sudo wget -N http://raw.github.com/mschmulen/raspberry-pi-dashboard/master/ibeacon
+sudo chmod 777 /etc/init.d/ibeacon
+sudo update-rc.d ibeacon defaults
+
+cd ~/
+
 # Chromium and dashboard stuff
 #unclutter &
 #sleep 30
 #chromium-browser --kiosk --ignore-certificate-errors --disable-restore-session-state http://www.woot.com &
 #x11vnc -forever
 
-#finally update the refresh script itself
-sudo wget -N http://raw.github.com/mschmulen/raspberry-pi-dashboard/master/refresh.sh
-
-sudo chmod +x refresh.sh
 
 echo "refresh done!"
