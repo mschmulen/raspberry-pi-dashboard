5b94c18deefb867a4341d03cfe4edb267d4e07a4
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-14 08:19:10 -0800

    force refresh

--- server/node-server/routes/index.js
+++ server/node-server/routes/index.js
@@ -37,18 +37,42 @@ function index(req, res){
   res.render('index', { title: 'dashboard-server', iPV4Interfaces:iPV4Interfaces });
 }
 
-
-function reactive(req, res){	
+function status(req, res){
+  
+  //process.env.PORT, process.env.IP
+  var ifaces=os.networkInterfaces();
+  
+  var iPV4Interfaces = [];
+  
+  for (var dev in ifaces) {
+    var alias=0;
 	
+    ifaces[dev].forEach(function(details){
+      if (details.family=='IPv4') {
+		  
+		  var obj = {
+		          address: details.address,
+		          family: details.family,
+				  name:dev
+		      };
+		  iPV4Interfaces.push(obj);
+		  
+          console.log(dev+(alias?':'+alias:''),details.address);
+          ++alias;
+      }
+    });
+  }//end for
+  
+  res.render('index', { title: 'dashboard-server', iPV4Interfaces:iPV4Interfaces });
+}//end status
+
+function dashboard(req, res){
 	res.render('index', { title: 'dashboard-server', iPV4Interfaces:iPV4Interfaces });
-}
-	
+}//end dashboard
 
-/**
- * Set up routes
- */
- 
+// Set up routes
 module.exports = function(app, options) {
   app.get('/', index);
-  app.get('/reactive', reactive);
+	app.get('/', status);
+  //app.get('/dashboard', dashboard);
 };
